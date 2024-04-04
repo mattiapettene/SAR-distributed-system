1f7060de5c84c3ba782e13672e4b9edd0b219fd8
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2024-04-04 12:00:02 +0200

    Upload main.m

---
+++ main.m
@@ -0,0 +1,20 @@
+%% Intelligent distributed system project
+% Pettene Mattia - Poggesi Gabriele
+
+%% Initialization
+
+clc;
+close all;
+clear;
+
+set(0,'defaulttextinterpreter','latex')
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+set(groot, 'defaultLegendInterpreter','latex');
+set(0,'DefaultFigureWindowStyle','docked');
+set(0,'defaultAxesFontSize',  22)
+set(0,'DefaultLegendFontSize', 20)
+
+addpath('functions/');
+
+
+%% Map creation
