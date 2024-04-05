e0b8cde31e29877124f836d2ad38dbacd653d58d
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2024-04-05 23:52:58 +0200

    Map 3D plot

--- main.m
+++ main.m
@@ -18,3 +18,34 @@ addpath('functions/');
 
 
 %% Map creation
+
+map = imread('usgsImageryLayer.tif');
+
+figure('Name','Real map')
+imshow('usgsImageryLayer.tif', 'InitialMagnification', 'fit');
+
+[x,y] = meshgrid(1:size(map,1), 1:size(map,2));
+z = double(map(:,:,1));
+
+% Plot using mesh
+figure;
+mesh(x,y,z);
+colorbar;
+
+% Plot using surf (best one)
+figure;
+surf(x, y, z, 'EdgeColor', 'none', 'FaceColor', 'texturemap');
+colorbar;
+
+%% Test changing resolution (coarser mesh)
+
+resolution = 200;
+
+new_x = linspace(1, size(map, 2), resolution); 
+new_y = linspace(1, size(map, 1), resolution); 
+new_z = interp2(x, y, double(map(:,:,1)), new_x', new_y);
+
+figure;
+mesh(new_x, new_y, new_z);
+colorbar;
+
