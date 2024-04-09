86369f914fce51e624de5d2c272dbf816da47e27
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2024-04-09 17:59:37 +0200

    First attempt obstacles addition

--- main.m
+++ main.m
@@ -20,7 +20,7 @@ addpath('Plot/');
 
 %% Map creation
 
-map = load("Rifugio_Viote.txt");
+map = load("torbiera_viote.txt");
 
 s = sqrt(length(map));
 H = zeros(s);
@@ -32,7 +32,7 @@ for y_i = 1:s
     end
 end
 
-H = flipud(H);
+
 
 figure('Name','3D map')
 hold on;
@@ -43,3 +43,34 @@ colorbar;
 daspect([1 1 0.5]);
 view(3);
 saveas(gcf, 'Plot/3D_map.eps', 'epsc')
+
+%% Obstacles addition
+
+nObstacles = 100;
+mapWidth = s;
+mapLength = s;
+z_obstacles = zeros(s, s);
+
+n = 0;
+while n < nObstacles
+    x_idx = randi([1 mapWidth],1);
+    y_idx = randi([1 mapLength],1);
+    z_obstacles(x_idx, y_idx) = H(x_idx, y_idx);
+    n = n+1;
+end
+
+[x, y] = find(z_obstacles);
+indices = sub2ind(size(z_obstacles), x, y);
+z = z_obstacles(indices);
+
+% Plot dei punti in 3D
+figure;
+hold on;
+mesh(H);
+colormap('summer');
+contour3(H, 80, 'k', 'LineWidth', 1);
+colorbar;
+daspect([1 1 0.5]);
+view(3);
+scatter3(x(:),y(:),z(:), 'filled', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r');
+grid on;
