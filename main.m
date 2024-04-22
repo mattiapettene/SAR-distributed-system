f40a696788dba8945140c6795cebea645e0394ff
Mattia Pettene <81318801+mattiapettene@users.noreply.github.com>
2024-04-22 12:06:37 +0200

    Added trees to 3D map, terrain slope and real trees location from satellite

--- main.m
+++ main.m
@@ -17,7 +17,6 @@ set(0,'DefaultLegendFontSize', 20)
 addpath('functions/');
 addpath('Plot/');
 
-
 %% Map creation
 
 map = load("torbiera_viote.txt");
@@ -43,3 +42,135 @@ colorbar;
 daspect([1 1 0.5]);
 view(3);
 saveas(gcf, 'Plot/3D_map.eps', 'epsc')
+
+%% Obstacles addition (red points)
+
+nObstacles = 100;
+mapWidth = s;
+mapLength = s;
+z_obstacles = zeros(s, s);
+
+n = 0;
+while n < nObstacles
+    x_idx = randi([1 mapWidth],1);
+    y_idx = randi([1 mapLength],1);
+    z_obstacles(x_idx, y_idx) = H(y_idx, x_idx);
+    n = n+1;
+end
+
+[x, y] = find(z_obstacles);
+z = z_obstacles(sub2ind(size(z_obstacles), x, y));
+
+% Plot dei punti in 3D
+figure('Name','Red points');
+hold on;
+mesh(H);
+colormap('summer');
+contour3(H, 80, 'k', 'LineWidth', 1);
+colorbar;
+daspect([1 1 0.5]);
+view(3);
+scatter3(x(:),y(:),z(:), 'filled', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r');
+hold off;
+grid on;
+
+%% Test function tree
+% [xT, yT, zT, xC, yC, zC] = tree(0,0,0);
+% figure;
+% surf(xT,yT,zT, 'FaceColor', '#53350A', "EdgeColor","none");
+% hold on;
+% axis equal;
+% surf(xC,yC,zC, 'FaceColor', '#2A7E19', "EdgeColor","none");
+% view(3)
+
+%% Obstacles addition (trees)
+
+nObstacles = 50;
+mapWidth = s;
+mapLength = s;
+xyzObstacles = cell(nObstacles, 1);
+
+n = 0;
+while n < nObstacles
+    x_idx = randi([1 mapWidth],1);
+    y_idx = randi([1 mapLength],1);
+    z_val = H(y_idx, x_idx);
+    [xT, yT, zT, xC, yC, zC] = tree(x_idx, y_idx, z_val);
+    xyzObstacles{n+1,1} = [xT(:), yT(:), zT(:)];
+    xyzObstacles{n+1,2} = [xC(:), yC(:), zC(:)];
+    n = n+1;
+end
+
+% Trees plot
+figure('Name','3D map with trees');
+hold on;
+
+mesh(H);
+colormap('summer');
+contour3(H, 80, 'k', 'LineWidth', 1);
+colorbar;
+daspect([1 1 0.5]);
+view(3);
+
+for i = 1:nObstacles
+   
+    xTrunk = xyzObstacles{i, 1}(:, 1);
+    yTrunk = xyzObstacles{i, 1}(:, 2);
+    zTrunk = xyzObstacles{i, 1}(:, 3);
+    
+    xCrown = xyzObstacles{i, 2}(:, 1);
+    yCrown = xyzObstacles{i, 2}(:, 2);
+    zCrown = xyzObstacles{i, 2}(:, 3);
+    
+    plot3(xTrunk, yTrunk, zTrunk, 'Color', '#53350A', 'LineWidth', 1);
+    plot3(xCrown, yCrown, zCrown, 'Color', '#2A7E19', 'LineWidth', 1);
+end
+
+grid on;
+axis equal;
+view(3);
+hold off;
+saveas(gcf, 'Plot/3D_map_trees.eps', 'epsc')
+
+%% Map gradient matrix
+
+[grady,gradx] = gradient(H);
+gradmod = sqrt(gradx.^2 + grady.^2);
+
+figure('Name','Terrain slope');
+contour(1:1:mapWidth,1:1:mapLength,H)
+hold on
+quiver(1:1:mapWidth,1:1:mapLength,gradx,grady)
+axis equal
+hold off
+saveas(gcf, 'Plot/terrain_slope.eps', 'epsc')
+
+% Satellite image in background
+
+RGB = imread('sat.png');
+RGB = imresize(RGB,[mapLength mapWidth]);
+
+I = im2double(RGB);
+
+figure('Name','Satellite slope');
+hold on
+image(gradmod,'CDataMapping','scaled')
+colorbar
+axis equal
+hImg = imshow(I); 
+set(hImg, 'AlphaData', 0.6);
+hold off
+
+%% From satellite to tree locations
+        
+[BW,maskedRGBImage] = WhereAreTree(RGB);
+
+figure('Name','Real trees location')
+hold on
+spy(double(BW))
+axis equal
+hImg = imshow(I); 
+set(hImg, 'AlphaData', 0.6);
+spy(BW);
+hold off
+saveas(gcf, 'Plot/real_trees_satellite.eps', 'epsc')
